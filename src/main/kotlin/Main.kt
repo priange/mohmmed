7834cec7b8c9f83b9be5c1c3e2a928a0feecd24c
priange <ityakup@gmail.com>
2023-02-02 21:50:07 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+fun main(args: Array<String>) {
+    var group = "Adalab"
+    println(group)
+    var person = "Mary"
+    person = "Jackson"
+    person = "Linda"
+    println(person)
+
+    var adalabNo = 33
+    adalabNo  = 27
+
+    val codehivePopulation = 100
+
+    var x = 1
+    println(x)
+
+    var letters: Byte = 26
+    var WorldCountries: Short = 195
+    var ugandaPop: Int = 45850000
+    var worldPop: Long = 7888000000
+    var gravity: Float = 9.8F
+    var pi: Double =3.4567124614256
+    var initial:Char = 'a'
+    var FullName: String = "come"
+    var DayTime = false
+    var myFriends = arrayOf("Mark","Jacob", "Steve")
+    var myFriendsAges = arrayOf(21,22,23,24)
+    var rwanda = arrayOf(13460000)
+
+
+
+
+
+
+
+
+
+
+
+
+   }
